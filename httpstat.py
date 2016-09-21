31e8b949922bcabcd6407043e6f345d31edd5941
yhan94 <hy-1994@hotmail.com>
2016-09-21 19:56:57 -0400

    Update httpstat.py

--- httpstat.py
+++ httpstat.py
@@ -43,10 +43,11 @@ https_template = """
 [   {a0000}  |     {a0001}    |    {a0002}    |      {a0003}      |      {a0004}     ]
              |                |               |                   |                  |
     namelookup:{b0000}        |               |                   |                  |
-                        connect:{b0001}       |                   |                  |
-                                    pretransfer:{b0002}           |                  |
-                                                      starttransfer:{b0003}          |
-                                                                                 total:{b0004}
+             |          connect:{b0001}       |                   |                  |
+             |                |     pretransfer:{b0002}           |                  |
+             |                |               |       starttransfer:{b0003}          |
+             |                |               |                   |              total:{b0004}
+[---{c0000}--|     {c0001}    |    {c0002}    |      {c0003}      |      {c0004}     ]
 """[1:]
 
 http_template = """
@@ -54,9 +55,10 @@ http_template = """
 [   {a0000}  |     {a0001}    |      {a0003}      |      {a0004}     ]
              |                |                   |                  |
     namelookup:{b0000}        |                   |                  |
-                        connect:{b0001}           |                  |
-                                      starttransfer:{b0003}          |
-                                                                 total:{b0004}
+             |           connect:{b0001}          |                  |
+             |                |       starttransfer:{b0003}          |
+             |                |                   |              total:{b0004}
+[   {c0000}  |     {c0001}    |      {c0003}      |      {c0004}     ]
 """[1:]
 
 
@@ -188,6 +190,15 @@ def main():
         range_transfer=d['time_total'] - d['time_starttransfer'],
     )
 
+    # calculat persentage
+    d.update(
+         per_dns=d['range_dns']/d['time_total']*100,
+         per_connection=d['range_connection']/d['time_total']*100,
+         per_ssl=d['range_ssl']/d['time_total']*100,
+         per_server=d['range_server']/d['time_total']*100,
+         per_transfer=d['range_transfer']/d['time_total']*100,
+    )
+
     # print header & body summary
     with open(headerf.name, 'r') as f:
         headers = f.read().strip()
@@ -235,6 +246,9 @@ def main():
     def fmtb(s):
         return cyan('{:<7}'.format(str(s) + 'ms'))
 
+    def fmtc(s):
+        return cyan('{:<7}'.format(str(s) + '%'))
+
     stat = template.format(
         # a
         a0000=fmta(d['range_dns']),
@@ -248,6 +262,12 @@ def main():
         b0002=fmtb(d['time_pretransfer']),
         b0003=fmtb(d['time_starttransfer']),
         b0004=fmtb(d['time_total']),
+        # c
+        c0000=fmtc('%.2f' %d['per_dns']),
+        c0001=fmtc('%.2f' %d['per_connection']),
+        c0002=fmtc('%.2f' %d['per_ssl']),
+        c0003=fmtc('%.2f' %d['per_server']),
+        c0004=fmtc('%.2f' %d['per_transfer']),
     )
     print()
     print(stat)
